c5eb77ec01ab4f62896954cdc795c3427888b92f
Yiyuan Li <yy@eyuan.me>
2025-03-09 17:12:15 +0800

    update sha

--- pew.rb
+++ pew.rb
@@ -6,7 +6,7 @@ class Pew < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/yuann3/pew/releases/download/v0.1.0/pew_0.1.0_darwin_arm64.tar.gz"
-      sha256 "6b5f0c7f1cdc125bc11ed7f07932b2b07b633cdad0c68508db5a7b2793e2e07e" 
+      sha256 "fb49f5a143a24505fde6f9415633e2b338d0f3b5801a09dc154388070e31a571" 
     else
       url "https://github.com/yuann3/pew/releases/download/v0.1.0/pew_0.1.0_darwin_amd64.tar.gz"
       sha256 "fb49f5a143a24505fde6f9415633e2b338d0f3b5801a09dc154388070e31a571"
@@ -16,7 +16,7 @@ class Pew < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/yuann3/pew/releases/download/v0.1.0/pew_0.1.0_linux_arm64.tar.gz"
-      sha256 "21628cafd7b10306739ce3dabb0528585bede7aca4473c9e0ba7671c9c592434"
+      sha256 "606f9fe25d3d90204be572d33b5d4d6cfe9c524f4bb74b02e30195e09f499e21"
     else
       url "https://github.com/yuann3/pew/releases/download/v0.1.0/pew_0.1.0_linux_amd64.tar.gz"
       sha256 "606f9fe25d3d90204be572d33b5d4d6cfe9c524f4bb74b02e30195e09f499e21"
