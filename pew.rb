ec202d0fd950cae7e55ee04ae67f46c1576de856
Yiyuan Li <yy@eyuan.me>
2025-04-23 18:36:21 +0800

    v0.1.2

--- pew.rb
+++ pew.rb
@@ -1,30 +1,30 @@
 class Pew < Formula
   desc "A lightweight CLI tool for dumping your codebase into a single file, perfect to work with LLMs "
   homepage "https://github.com/yuann3/pew"
-  version "0.1.1"
+  version "0.1.2"
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yuann3/pew/releases/download/v0.1.1/pew_0.1.1_darwin_arm64.tar.gz"
-      sha256 "2724698d7700f97556fa5a98d0a974aa2da48080e6d2f2010c04bcaa825d8896" 
+      url "https://github.com/yuann3/pew/releases/download/v0.1.2/pew_0.1.2_darwin_arm64.tar.gz"
+      sha256 "7dc74327a2266c29bc6457f5621548539cf22143a5aba0bcca4da7b59e5d75d3" 
     else
-      url "https://github.com/yuann3/pew/releases/download/v0.1.1/pew_0.1.1_darwin_amd64.tar.gz"
-      sha256 "f810cf0bb9d27621095fe11d708ae941c8e55bdac1eab547058ba5b2f259bf08"
+      url "https://github.com/yuann3/pew/releases/download/v0.1.2/pew_0.1.2_darwin_amd64.tar.gz"
+      sha256 "ddd184e7343389d9ad8e9901e87b550ba7341422fec4254d7006fa7d2fa84321"
     end
   end
   
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/yuann3/pew/releases/download/v0.1.1/pew_0.1.1_linux_arm64.tar.gz"
-      sha256 "8d5814cbb94e37a1e6a0cd89ef4c9a3825e2b43a1f3baea52ddca8f4f0266c9f"
+      url "https://github.com/yuann3/pew/releases/download/v0.1.2/pew_0.1.2_linux_arm64.tar.gz"
+      sha256 "c235a72ebdd7e814e0942f5f3030a5919656755972c46ee235373563966ab8e6"
     else
-      url "https://github.com/yuann3/pew/releases/download/v0.1.1/pew_0.1.1_linux_amd64.tar.gz"
-      sha256 "2e935ba3ece418183f11941d688e090d783b188f88a9059bf11ff872e6518380"
+      url "https://github.com/yuann3/pew/releases/download/v0.1.2/pew_0.1.2_linux_amd64.tar.gz"
+      sha256 "7136b531d90fa3f922ec8d9de742219de8e0f3c76ab73287ce70e609a7a54c99"
     end
   end
   
   def install
-    bin.install "pew_0.1.1_#{OS.mac? ? 'darwin' : 'linux'}_#{Hardware::CPU.arm? ? 'arm64' : 'amd64'}" => "pew"
+    bin.install "pew_0.1.2_#{OS.mac? ? 'darwin' : 'linux'}_#{Hardware::CPU.arm? ? 'arm64' : 'amd64'}" => "pew"
   end
 
   test do
